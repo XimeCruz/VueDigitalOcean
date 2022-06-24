ea6747f8e82cb0df2610fd80d3dc9c06ebb6c185
XimeCruz <xmenacruzt@gmail.com>
2022-06-24 02:18:06 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app      
+      color="white"
+      dark
+      elevation="0"
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="@/assets/logo_main.svg"
+          transition="scale-transition"
+          width="40"
+        />
+        <v-app-bar-title class="primary--text"
+        >SuperTicket</v-app-bar-title>
+      </div>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style>
+  html { overflow-y: auto !important }
+</style>
